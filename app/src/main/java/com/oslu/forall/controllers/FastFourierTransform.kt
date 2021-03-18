24856ae05dfddb6b3c504d27f397299a292972b5
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-03-18 14:24:14 -0300

    Basic structure for FFT and signals

---
+++ app/src/main/java/com/oslu/forall/controllers/FastFourierTransform.kt
@@ -0,0 +1,10 @@
+package com.oslu.forall.controllers
+
+class FastFourierTransform {
+
+    // TODO: Equivalent to the FFT class in the old code (ServicoAudio.java)
+
+    fun fft(n: Int) {
+        // TODO
+    }
+}
---
+++ app/src/main/java/com/oslu/forall/controllers/SignalController.kt
@@ -0,0 +1,14 @@
+package com.oslu.forall.controllers
+
+class SignalController {
+
+    // TODO: Use this class to make the signals (flashlight and vibration)
+
+    fun vibrate() {
+        // TODO
+    }
+
+    fun flashlight() {
+        // TODO
+    }
+}
